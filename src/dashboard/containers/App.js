2be7f2570a4e1dd8aace71581c61e05023a2b3d3
Dustin Kahn <dustin@dustinkahn.com>
2017-04-24 20:50:32 -0400

    set up dashboard section with appropriate folder structure, almost completed boiler plate for redux with actions, reducers, store, and fetched api

--- src/dashboard/containers/App.js
+++ src/dashboard/containers/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getGiphyProfile } from '../actions/dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <div className="app-header">
-          <h2>Welcome Mike Butt</h2>
+          <h2>Welcome Mike</h2>
         </div>
       </div>
     );
---
+++ src/dashboard/reducers/dashboard.js
@@ -0,0 +1,11 @@
+import { FETCH_GIPHY_PROFILE_SUCCESS } from '../actions/dashboard';
+
+export const giphyProfileReducer = (state, action) => {
+  switch (action.type) {
+    case FETCH_GIPHY_PROFILE_SUCCESS:
+      let newState = { giph: action.giph };
+      return newState;
+    default:
+      return state;
+  }
+}

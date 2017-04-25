37b3bcd8db9f06fdcbaae5b7471809bbf2aeabd4
Dustin Kahn <dustin@dustinkahn.com>
2017-04-24 20:50:32 -0400

    set up dashboard section with appropriate folder structure, almost completed boiler plate for redux with actions, reducers, store, and fetched api

--- src/main.js
+++ src/main.js
@@ -2,19 +2,14 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import App from './components/App';
-import thunk from 'redux-thunk';
-import App from './components/App';
-import reducers from './reducers/combinedReducers';
+import App from './dashboard/containers/App';
+import reducers from './sharedResources/rootReducer';
 
 let store = createStore(reducers, applyMiddleware(thunk));
 
-$(function() {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  );
-});
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
--- src/reducers/combinedReducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-let reducers = combineReducers({
-
-});
-
-export default reducers; 
